c62f2d721e016ad52a64cbc7101b4642bae42bd7
noal24sl <noal24sl@student.ju.se>
2024-11-22 15:05:46 +0100

    Trying to understand Classes

--- game.js
+++ game.js
@@ -1,7 +1,33 @@
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(800, 500);
 }
 
+class User {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    // this.size = 100;
+  }
+
+  draw() {
+    fill(255, 255, 0);
+    ellipse(this.x, this.y, 100);
+  }
+}
+
+let user = new User(100, 100);
+// let anotherUser = new User(200, 200);
+
 function draw() {
-  background(255, 140, 0);
+  background(51, 53, 135);
+
+  user.draw();
+  /*
+  // moving the user
+  user.x += 1;
+
+  anotherUser.draw();
+  // moving the user
+  anotherUser.y += 1;
+  */
 }
