a88c47b59248108976e4e58836cab3b2a493f63d
noal24sl <noal24sl@student.ju.se>
2024-11-25 17:41:46 +0100

    trying to put in the character and beginning on platforms

--- js/game.js
+++ js/game.js
@@ -1,5 +1,11 @@
+// global variables
+let userImage;
+
 function setup() {
   createCanvas(800, 500);
+
+  // Load the character
+  userImage = loadImage("assets/user.png");
 }
 
 class User {
@@ -10,24 +16,41 @@ class User {
   }
 
   draw() {
-    fill(255, 255, 0);
-    ellipse(this.x, this.y, 100);
+    image(userImage, this.x, this.y, 100, 100);
   }
 }
 
 let user = new User(100, 100);
 // let anotherUser = new User(200, 200);
 
+class Platform {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    noStroke();
+    fill(232, 23, 136);
+    rect(this.x, this.y, 80, 10, 10);
+  }
+}
+
+let platform = new Platform(100, 100);
+
 function draw() {
   background(51, 53, 135);
 
   user.draw();
-  /*
-  // moving the user
-  user.x += 1;
 
+  /*
   anotherUser.draw();
   // moving the user
   anotherUser.y += 1;
   */
+
+  // moving the platform
+  platform.x += 1;
+
+  platform.draw();
 }
