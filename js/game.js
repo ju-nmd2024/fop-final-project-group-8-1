b6ed3d02ae7812069649553306baaba179cab03b
nivinmuhamed <muni24by@student.ju.se>
2024-11-27 10:12:47 +0100

    fixing stuff, trying to get our character and platforms to show

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ let userImage;
 let gameState = false;
 
 function preload() {
-  userImage = loadImage("assets/user.png");
+  userImage = loadImage("./assets/user.png");
 }
 
 window.preload = preload;
@@ -20,27 +20,38 @@ window.setup = setup;
 let user = new User(100, 100, userImage);
 // let anotherUser = new User(200, 200);
 
-let startGame = new StartGame(0, 0);
+let platform = new Platform(100, 100);
+
+let startGame = new StartGame(100, 100);
 
 function draw() {
   if (!gameState) {
     startGame.draw();
   } else {
     background(51, 53, 135);
+    platform.draw();
 
-    user.draw();
+    // user.draw();
     /*
     anotherUser.draw();
     // moving the user
     anotherUser.y += 1;
     */
 
-    platform.draw();
     // moving the platform
     //platform.x += 1;
   }
 
-  image(userImage, 100, 100, 100, 100);
+  // image(userImage, 100, 100, 100, 100);
+
+  // // collision
+  // if(
+  //   dist(user) < platform
+  // ) {
+  //   console.log("collision");
+  // } else {
+  //   console.log("free");
+  // }
 }
 window.draw = draw;
 
--- js/platform.js
+++ js/platform.js
@@ -1,6 +1,7 @@
 export default class Platform {
-  constructor(w, y) {
-    this.x = Math.random() * w;
+  constructor(x, y) {
+    // this.x = Math.random() * 420;
+    this.x = x;
     this.y = y;
   }
 
@@ -9,6 +10,14 @@ export default class Platform {
     fill(232, 23, 136);
     rect(this.x, this.y, 80, 10, 10);
   }
+  hitTest(x, y) {
+    return (
+      x > this.x &&
+      x < this.x + this.width &&
+      y > this.y &&
+      y < this.y + this.height
+    );
+  }
 }
 
-let platform = new Platform(100, 100);
+// let platform = new Platform(100, 100);
